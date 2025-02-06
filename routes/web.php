1e84d4c3ccb23d4b654c5051f2a9b7979ea7b5b8
Nyxora <nyxora1@gmail.com>
2025-02-06 07:56:28 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,13 @@ use App\Http\Controllers\RoutingController;
 use Illuminate\Support\Facades\Route;
 
 
-// Route::get('', [RoutingController::class, 'index'])->name('root');
-// Route::get('{first}/{second}/{third}', [RoutingController::class, 'thirdLevel'])->name('third');
-// Route::get('{first}/{second}', [RoutingController::class, 'secondLevel'])->name('second');
-// Route::get('{any}', [RoutingController::class, 'root'])->name('any');
-// Route::get('{first}/{second}/{third}/{id}', [RoutingController::class, 'fourthLevel'])->name('fourth');
+Route::get('', [RoutingController::class, 'index'])->name('root');
+Route::get('{first}/{second}/{third}', [RoutingController::class, 'thirdLevel'])->name('third');
+Route::get('{first}/{second}', [RoutingController::class, 'secondLevel'])->name('second');
+Route::get('{any}', [RoutingController::class, 'root'])->name('any');
+Route::get('{first}/{second}/{third}/{id}', [RoutingController::class, 'fourthLevel'])->name('fourth');
 
 
-Route::get('/editor', function () {
-    return view('user-pages.editor');
-})->name('editor');
+// Route::get('/editor', function () {
+//     return view('user-pages.editor');
+// })->name('editor');
