4e1e71194bf8b1cccc6b1972fd42c7b6a4df4917
Nyxora <nyxora1@gmail.com>
2025-02-12 09:35:53 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -23,17 +23,9 @@ Route::middleware([CheckRole::class . ':admin'])->group(function () {
 });
 
 Route::middleware([CheckRole::class . ':user'])->group(function () {
+    Route::get('/index', [RoutingController::class, 'index'])->name('user-index')->middleware(CheckRole::class . ':guest');
     Route::get('{any}', [RoutingController::class, 'root'])->name('any');
     Route::get('{first}/{second}', [RoutingController::class, 'secondLevel'])->name('second');
     Route::get('{first}/{second}/{third}', [RoutingController::class, 'thirdLevel'])->name('third');
     Route::get('{first}/{second}/{third}/{id}', [RoutingController::class, 'fourthLevel'])->name('fourth');
 });
-
-// Route::get('', [RoutingController::class, 'index'])->name('root');
-
-
-
-
-// Route::get('/editor', function () {
-//     return view('user-pages.editor');
-// })->name('editor');
