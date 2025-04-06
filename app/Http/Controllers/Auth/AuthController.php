650f62308fb36fd561af8cfc6fed338608459a4c
Nyxora <nyxora1@gmail.com>
2025-04-07 02:03:14 +0700

    Update AuthController.php

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -35,7 +35,7 @@ class AuthController extends Controller
             }
 
             // Create an HTTP-only cookie for access token
-            $cookie = cookie('bearer_token', $data['token'], 60, '/', 'aifigu.azurewebsites.net', true, true);
+            $cookie = cookie('bearer_token', $data['token'], 60, '/', null, true, true);
             Cookie::queue($cookie);
 
             // Fetch user role after login using the access_token
